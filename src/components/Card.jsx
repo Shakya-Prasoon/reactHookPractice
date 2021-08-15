3032fdfa92736fcdae268882b476d40d29657693
Prasoon Shakya <pras95shakya@gmail.com>
2021-08-14 22:50:02 -0700

    testing

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -3,7 +3,8 @@ const Card = (props) => {
     const {employer, jobTitle, universityName, uuid} = props
     // console.log(employer, jobTitle, universityName);
     
-    return (<span key= {uuid}>
+    return (<span 
+                key= {uuid}>
 					{employer}, &nbsp; 
 					{jobTitle} - &nbsp; 
 					{universityName}
